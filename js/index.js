b320b3c1ea4fa89936ba3b2e8d284f26e640ad7d
Diego <diegojimenezcastro2001@gmail.com>
2026-08-25 16:50:15 +0200

    resuelto

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var botonSi = document.getElementById("botonSi");
 var botonNo = document.getElementById("botonNo");
 
@@ -12,19 +11,4 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault();
         window.location.href = "inicio.html";
     });
-=======
-var botonSi = document.getElementById("botonSi");
-var botonNo = document.getElementById("botonNo");
-
-document.addEventListener('DOMContentLoaded', function() {
-    botonSi.addEventListener("click", async(event) => {
-        event.preventDefault();
-        window.location.href = "password.html";
-    });
-
-    botonNo.addEventListener("click", async(event) => {
-        event.preventDefault();
-        window.location.href = "inicio.html";
-    });
->>>>>>> 9989992ca0eb607f8d858f57431169d8d0bba6ad
-});
+})
